e87851e0396a6e37fd282d1919a5ce9e3d4086f7
JC <10779067+jancschubert@users.noreply.github.com>
2024-02-28 09:48:35 +0100

    Add painting functionality * mousedown state to painting as we drag the cursor * prevent drag event * just paint straight black for now

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,19 @@
 let pixelCanvas;
+let mousedown = false;
 
 window.addEventListener('DOMContentLoaded', e => {
     let pixelCanvas = document.querySelector('#pixel-canvas');
+    pixelCanvas.addEventListener('mousedown', e => { mousedown = true; });
+    pixelCanvas.addEventListener('mouseup', e => { mousedown = false; });
+
     createGrid(pixelCanvas, 33);
 });
 
 function createPixel() {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
+    pixel.addEventListener('mouseover', onPixelHovered);
+    pixel.addEventListener('dragstart', e => e.preventDefault());
     return pixel;
 }
 
@@ -17,4 +23,11 @@ function createGrid(parent, size) {
         parent.append(createPixel());
     }
     parent.style.width = `${size*16 + 2}px`;
+}
+
+function onPixelHovered(e) {
+    e.preventDefault();
+    if (mousedown) {
+        e.target.style.background = 'black';
+    }
 }
