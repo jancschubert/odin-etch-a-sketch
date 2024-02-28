2c50cfcd0e051dd539895e881446d6d6727c10af
JC <10779067+jancschubert@users.noreply.github.com>
2024-02-28 09:08:22 +0100

    Create a static 5x5 pixel grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,9 @@
 window.addEventListener('DOMContentLoaded', e => {
     console.log('loaded');
 });
+
+
+// we need a way to create a pixel via js
+// we need a way to create a grid of a defined size via js
+    // add the right amount of pixels
+    // set the width on pixel-canvas so a square canvas is created
